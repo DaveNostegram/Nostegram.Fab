a15add97c41b0e756646d8fa20daef7de97d25df
Dave Nostegram <dmargetson05@gmail.com>
2026-08-14 01:25:24 +0100

    Fix to test

--- src/Nostegram.Fab.Contracts/Cards/CardWriteDto.cs
+++ src/Nostegram.Fab.Contracts/Cards/CardWriteDto.cs
@@ -1,8 +1,15 @@
+using Nostegram.Fab.Contracts.Normalisations;
+
 namespace Nostegram.Fab.Contracts.Cards;
 
 public class CardWriteDto
 {
-    public required string Name { get; set; }
+    private string _name = null!;
+    public required string Name
+    {
+        get => _name;
+        set => _name = NameNormaliser.ForDisplay(value);
+    }
     public List<CardVariantWriteDto> CardVariants { get; set; } = [];
     public List<Guid> Talents { get; set; } = [];
     public List<Guid> CardTypes { get; set; } = [];
